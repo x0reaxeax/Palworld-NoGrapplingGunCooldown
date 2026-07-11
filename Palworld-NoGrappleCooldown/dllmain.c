5cf399964bd6bf11c0c91a4cac73f155a261830c
x0reaxeax <xoreaxeax96@gmail.com>
2026-07-11 12:52:14 +0200

    Updated for Palworld 1.0+

--- Palworld-NoGrappleCooldown/dllmain.c
+++ Palworld-NoGrappleCooldown/dllmain.c
@@ -1,48 +1,104 @@
-// For Palworld v0.5.1.68353
+// For Palworld v1.0.0.100427
 // github.com/x0reaxeax
 
 #include <Windows.h>
 
 #define MAYBE_UNUSED
 
+typedef WORD UMASK16, *PUMASK16, *LPUMASK16;
+typedef UMASK16 UMASK, *PUMASK, *LPUMASK;
+typedef UINT64 QWORD64, *PQWORD64, *LPQWORD64;
+
+typedef struct _SEARCH_INFO {
+    LPCVOID lpSearchBase;
+    SIZE_T dwSearchSize;
+} SEARCH_INFO, *PSEARCH_INFO, *LPSEARCH_INFO;;
+
+CONST UMASK awSignature[] = {
+    0x0F, 0x2F, 0xB7, '??', '??', 0x00, 0x00,   // comiss xmm6,[rdi+???]
+    0x72, '??',                                 // jb short +???
+    0x48, 0x8D                                  // lea64
+};
+
+#define JMP_OFFSET  0x7U
+
+MAYBE_UNUSED LPCBYTE SearchForMaskedSignature(
+    _In_ LPCVOID lpSearchBase,
+    _In_ SIZE_T dwSearchSize,
+    _In_ PUMASK awSignature,
+    _In_ SIZE_T dwSignatureSize
+) {
+    for (SIZE_T i = 0; i < dwSearchSize - dwSignatureSize; i++) {
+        BOOLEAN bMatch = TRUE;
+        for (SIZE_T j = 0; j < dwSignatureSize; j++) {
+            if ('??' != awSignature[j] && ((LPCBYTE) lpSearchBase)[i + j] != (BYTE) awSignature[j]) {
+                bMatch = FALSE;
+                break;
+            }
+        }
+        if (bMatch) {
+            return (LPCBYTE) lpSearchBase + i;
+        }
+    }
+    return NULL;
+}
+
+MAYBE_UNUSED static BOOLEAN GetSearchInfo(
+    _Out_ LPSEARCH_INFO lpSearchInfo
+) {
+    if (NULL == lpSearchInfo) {
+        return FALSE;
+    }
+
+    ZeroMemory(lpSearchInfo, sizeof(SEARCH_INFO));
+
+    HMODULE hModule = GetModuleHandleA(NULL);
+    if (NULL == hModule) {
+        return FALSE;
+    }
+
+    PIMAGE_DOS_HEADER pDosHeader = (PIMAGE_DOS_HEADER) hModule;
+    if (IMAGE_DOS_SIGNATURE != pDosHeader->e_magic) {
+        return FALSE;
+    }
+
+    PIMAGE_NT_HEADERS pNtHeaders = \
+        (PIMAGE_NT_HEADERS) ((LPBYTE) hModule + pDosHeader->e_lfanew);
+    if (IMAGE_NT_SIGNATURE != pNtHeaders->Signature) {
+        return FALSE;
+    }
+
+    lpSearchInfo->lpSearchBase = (LPCVOID) hModule;
+    lpSearchInfo->dwSearchSize = pNtHeaders->OptionalHeader.SizeOfImage;
+    return TRUE;
+}
+
 MAYBE_UNUSED LPBYTE AOBScan(
     VOID
 ) {
     CONST SIZE_T ccbComissInstSize = sizeof(
         (BYTE[]) {
-        0x0F, 0x2F, 0xB6, 0x98, 0x04, 0x00, 0x00
+        0x0F, 0x2F, 0xB7, 0x78, 0x05, 0x00, 0x00
     }
-    );                                              // comiss xmm6, dword ptr [rsi+498h]
+    );                                              // comiss xmm6,[rdi+00000578]
 
     BYTE abTargetBytesComissXmm6[] = {
-        0x0F, 0x2F, 0xB6                            // comiss xmm6, dword ptr [rsi+???]
+        0x0F, 0x2F, 0xB7                            // comiss xmm6, dword ptr [rsi+???]
     };
 
     BYTE abTargetBytesJbShort[] = {
-        0x72, 0x20                                  // jb short +0x20
+        0x72, 0x5C                                  // jb short +0x5C
     };
 
-    HMODULE hModule = GetModuleHandleA(NULL);
-    if (NULL == hModule) {
+    SEARCH_INFO searchInfo = { 0 };
+    if (!GetSearchInfo(&searchInfo)) {
         return NULL;
     }
 
-    PIMAGE_DOS_HEADER pDosHeader = (PIMAGE_DOS_HEADER) hModule;
-    if (IMAGE_DOS_SIGNATURE != pDosHeader->e_magic) {
-        return NULL;
-    }
-
-    PIMAGE_NT_HEADERS pNtHeaders = \
-        (PIMAGE_NT_HEADERS) ((LPBYTE) hModule + pDosHeader->e_lfanew);
-
-    if (IMAGE_NT_SIGNATURE != pNtHeaders->Signature) {
-        return NULL;
-    }
-
-    SIZE_T dwSizeOfImage = pNtHeaders->OptionalHeader.SizeOfImage;
-    LPBYTE lpStart = (LPBYTE) hModule;
+    SIZE_T dwSizeOfImage = searchInfo.dwSearchSize;
+    LPBYTE lpStart = (LPBYTE) searchInfo.lpSearchBase;
     LPBYTE lpEnd = lpStart + dwSizeOfImage;
-
+#ifdef _OLD_STAIC_SEARCH
     for (
         LPBYTE lpAddress = lpStart;
         lpAddress < lpEnd - ccbComissInstSize + sizeof(abTargetBytesJbShort);
@@ -62,61 +118,47 @@ MAYBE_UNUSED LPBYTE AOBScan(
             }
         }
     }
-
+    
     return NULL;
+#else
+    return (LPBYTE) SearchForMaskedSignature(
+        searchInfo.lpSearchBase,
+        searchInfo.dwSearchSize,
+        awSignature,
+        ARRAYSIZE(awSignature)
+    );
+#endif // _OLD_STAIC_SEARCH
 }
 
 BOOL PatchCooldownTimer(
     VOID
 ) {
-    DWORD64 qwCooldownTimerOffset = 0x2F2D223;
-
-    CONST BYTE abOriginalBytes[] = {
-        0x72, 0x20  // jb short +0x20
+    CONST BYTE abPatchBytes[] = {
+        0xEB, 0x00      // jmp short +0x00
     };
 
-    CONST BYTE abPatchBytes[sizeof(abOriginalBytes)] = {
-        0xEB, 0x00  // jmp short +0x00
-    };
-
-    LPBYTE lpBaseAddress = (LPBYTE) GetModuleHandle(NULL);
-    if (NULL == lpBaseAddress) {
+    LPVOID lpCooldownTimer = AOBScan();
+    if (NULL == lpCooldownTimer) {
         return FALSE;
     }
 
-    LPBYTE lpCooldownTimer = lpBaseAddress + qwCooldownTimerOffset;
-
     MEMORY_BASIC_INFORMATION mbi = { 0 };
-    BOOL bValidMemory = TRUE;
     if (0 == VirtualQuery(
         lpCooldownTimer,
         &mbi,
         sizeof(mbi)
     )) {
-        bValidMemory = FALSE;
+        return FALSE;
     } else {
         if (MEM_COMMIT != mbi.State || PAGE_EXECUTE_READ != mbi.Protect) {
-            bValidMemory = FALSE;
-        }
-    }
-
-    if (!bValidMemory || EXIT_SUCCESS != memcmp(
-        lpCooldownTimer,
-        abOriginalBytes,
-        sizeof(abOriginalBytes)
-    )) {
-        lpCooldownTimer = AOBScan();
-        if (NULL == lpCooldownTimer) {
             return FALSE;
         }
-
-        lpCooldownTimer += 7;
     }
 
     DWORD dwOldProtect;
     if (!VirtualProtect(
-        lpCooldownTimer,
-        sizeof(abOriginalBytes),
+        (LPVOID) ((QWORD64) lpCooldownTimer + JMP_OFFSET),
+        sizeof(abPatchBytes),
         PAGE_EXECUTE_READWRITE,
         &dwOldProtect
     )) {
@@ -124,14 +166,14 @@ BOOL PatchCooldownTimer(
     }
 
     memcpy(
-        lpCooldownTimer,
+        (LPVOID) ((QWORD64) lpCooldownTimer + JMP_OFFSET),
         abPatchBytes,
         sizeof(abPatchBytes)
     );
 
     if (!VirtualProtect(
-        lpCooldownTimer,
-        sizeof(abOriginalBytes),
+        (LPVOID) ((QWORD64) lpCooldownTimer + JMP_OFFSET),
+        sizeof(abPatchBytes),
         dwOldProtect,
         &dwOldProtect
     )) {
